f276e5e7fd00b89a1b3decfdfb669e7505ae3850
borahdev <mailtohimanshuborah@gmail.com>
2026-06-14 14:49:10 +0000

    Updated config

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ import os
 COMFY_URL = os.environ.get("COMFY_URL", "http://127.0.0.1:8000")
 COMFY_PUBLIC_URL = os.environ.get(
     "COMFY_PUBLIC_URL",
-    "https://8000-01ktneb7ga04t01deg8y7q4gsh.cloudspaces.litng.ai"
-)
+    "https://example.cloudspaces.litng.ai"
+)  # set env, dont update directly
